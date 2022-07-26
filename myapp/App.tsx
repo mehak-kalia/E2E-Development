686459285b1805740d9c7bca0fad375564ff3fa7
mehak-kalia <mehakkalia77@gmail.com>
2022-07-26 14:35:42 +0530

    LinkedIn Jobs FlatList implementation

--- myapp/App.tsx
+++ myapp/App.tsx
@@ -1,63 +1,88 @@
 import { StatusBar } from "expo-status-bar";
 import { Component, useState } from "react";
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
-// App is Child Class
-// Component is Parent Class
-export default class App extends Component{
- 
-  // Attrbutes: Property of Object
-  idx = 0;
-  quotes = [
-    "Be Exceptional",
-    "Search the Candle rather than cursing the darkness",
-    "Work hard, Be Successful",
-    "Your future is all about connecting the dots in past",
-    "Work with joy and fill in joy"
-  ];
 
-  state = {
-    quote: this.quotes[this.idx]
-  }
 
-  onButtonPressed = () => {
-   
-    this.idx++;
+const LinkedInJobs = [
+  {
+    name: "Training Program",
+    company: "Coincent.ai",
+    location: "Mumbai",
+    type: "Remote",
+    logo: "https://coincent.ai/images/Coincent_PNG.png"
+  },
 
-    if(this.idx == this.quotes.length-1){
-      this.idx = 0;
-    }
+  {
+    name: "Web Development Intern",
+    company: "Corizo",
+    location: "Delhi",
+    type: "On Site",
+    logo: "https://corizo.in/wp-content/uploads/2021/12/cropped-Corizo-2-1-316x190.png"
+  },
 
-    if(this.idx < 0){
-      this.idx = this.quotes.length-1;
-    }
+  {
+    name: "Data Science Intern",
+    company: "Sabudh Foundation",
+    location: "Ludhiana",
+    type: "On Site",
+    logo: "https://sabudh.org/wp-content/themes/hello-theme-child-master/images/logo.png"
+  },
+  {
+    name: "Graphic Design Intern",
+    company: "Alchemial",
+    location: "Bangalore",
+    type: "Remote",
+    logo: "https://alchemial.com/images-duplicate/logo-white.png"
+  },
+  {
+    name: "Flutter Developer",
+    company: "Accenture",
+    location: "Chandigarh",
+    type: "On Site",
+    logo: "https://www.accenture.com/t20180820T081710Z__w__/in-en/_acnmedia/Accenture/Dev/Redesign/Acc_Logo_Black_Purple_RGB.PNG"
+  },
+
+]
+
+
+// List Item Layout
+const Item = (itemData:any) => (
+  <View style={styles.item}>
     
-    // Refresh the UI with new Value of quote
-    this.setState(
-      {
-        quote: this.quotes[this.idx]
-      }
-    );
-  }
+         <img   style={{
+            height: 50,
+            width: 120,
+            flexDirection: "row"
+          }}
+          src= {itemData.logo}></img>
+     <Text style={styles.title} >{itemData.name}</Text>
+     <Text style={styles.subTitle} >{itemData.company}</Text>
+     <Text style={styles.small} >{itemData.location}</Text>
+     <Text style={styles.small} >{itemData.type}</Text>
 
-  render(){
-    return (
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Text style={styles.textStyle} >Welcome to Class Component</Text>
-        <Text>{this.state.quote}</Text>
-        <Button title='NEXT' onPress={this.onButtonPressed}></Button>
-      </View>
-    );
-  }
+  </View>
+);
+
+// Specified to execute renderItem function and create Item Views
+const renderItem = ({item}:any) => Item(item);
+
+// Functional Component
+export default function App() {
+
+  return (
+    <View style={styles.background}>
+      <StatusBar style="auto" />
+      <FlatList data={LinkedInJobs} renderItem={renderItem}/>
+    </View>
+  );
 
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+   
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -66,6 +91,43 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: "#f00",
     marginBottom: 20
+  },
+
+  background:{
+    
+    fontSize: 24,
+    marginBottom: 20,
+    margin: 20,
+    alignContent: "center"
+  
+  },
+
+  item: {
+    borderColor: 'grey',
+    borderWidth: 5,
+    backgroundColor: '#fff',
+    padding: 8,
+    margin: 6,
+    width: 300,
+    alignContent: "center",
+    justifyContent: 'center'
+    
+  },
+
+  title: {
+    fontSize: 16,
+    color: '#000000',
+    flexDirection: "row"
+  },
+
+  subTitle: {
+    fontSize: 12,
+    color: 'grey'
+  },
+
+  small : {
+    fontSize: 10,
+    color: 'grey'
   }
 
 });
